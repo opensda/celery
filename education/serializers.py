8f895c1bac242eefb741e0c977c2acb8f5b8175c
Sudarev_D <89365265993@mail.ru>
2023-10-26 17:46:17 +0700

    add viewset for Course model, add generics for Lesson model

--- education/serializers.py
+++ education/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
-from education.models import Course
+from education.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
+
+
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
         fields = '__all__'
 
 
--- education/urls.py
+++ education/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from education.apps import EducationConfig
 from rest_framework.routers import DefaultRouter
 
-from education.views import CourseViewSet
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = EducationConfig.name
 
@@ -10,5 +13,20 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
+    path('lesson/create', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 
 ] + router.urls
+
+
+
+
+
+
+
+
+
+
